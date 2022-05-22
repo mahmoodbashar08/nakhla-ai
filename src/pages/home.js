54fee8867214060fa9799c8da91f5c3feddfce20
Tawfek .M <tawfekaltaae3@gmail.com>
2022-05-23 00:07:08 +0300

    add pages

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Home extends React.Component{
+
+
+    render(){
+
+        return(
+            <h1>im home</h1>
+        )
+    }
+}
+
+
+export default Home ;
---
+++ src/pages/map.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Map extends React.Component{
+
+
+    render(){
+
+        return(
+            <h1>im Map</h1>
+        )
+    }
+}
+
+
+export default Map ;
